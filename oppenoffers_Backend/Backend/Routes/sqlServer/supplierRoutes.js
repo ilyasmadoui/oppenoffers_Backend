09854625fcd346bc7b171dbff6c1be7310498133
ilyasmadoui <ilyasmadoui2020@gmail.com>
2025-12-09 19:41:34 +0100

    Delete oppenoffers_Backend/Backend directory

--- oppenoffers_Backend/Backend/Routes/sqlServer/supplierRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const supplierController = require('../../Controllers/sqlServer/supplierController');
-
-router.post('/addSupplier', supplierController.addSupplier);
-
-module.exports = router;
